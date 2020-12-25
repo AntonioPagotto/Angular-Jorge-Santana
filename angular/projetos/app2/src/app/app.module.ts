f75c9aa19fef7e99205344191d17006c5d5e853e
antoniopagotto121@gmail.com <antoniopagotto121@gmail.com>
2020-12-25 14:19:15 -0300

    layoutComponents

--- angular/projetos/app2/src/app/app.module.ts
+++ angular/projetos/app2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
