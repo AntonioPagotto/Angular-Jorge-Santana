92f5176ed48ea916a9056f13465912901d78d161
antoniopagotto121@gmail.com <antoniopagotto121@gmail.com>
2020-12-26 17:10:40 -0300

    TabsAtOfertaComponent

--- angular/projetos/app2/src/app/app.module.ts
+++ angular/projetos/app2/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -28,6 +28,7 @@ import { OfertaComponent } from './layout/oferta/oferta.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    NgbNavModule,
     HttpClientModule,
   ],
   providers: [],
