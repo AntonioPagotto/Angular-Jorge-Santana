50e5d5b6333501316e788842b1b87bb60aec95da
antoniopagotto121@gmail.com <antoniopagotto121@gmail.com>
2020-12-30 20:21:43 -0300

    Two way data binding

--- angular/projetos/app2/src/app/layout/pages/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/layout/pages/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  endereco: string = 'Rua XYZ';
+  numero: number = 10;
+  complemento: string = 'casa rosa';
+  formaPagamento: string = 'debito';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  atualizaEndereco(endereco: string){
+    this.endereco = endereco;
+  }
+  atualizaNumero(numero: number){
+    this.numero = numero;
+  }
+  atualizaComplemento(complemento: string){
+    this.complemento = complemento;
+  }
+  atualizaFormaPagamento(formaPagamento: string){
+    this.formaPagamento = formaPagamento;
+  }
 }
