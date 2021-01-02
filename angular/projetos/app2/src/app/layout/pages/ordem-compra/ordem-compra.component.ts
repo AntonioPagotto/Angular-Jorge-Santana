4269fc5c2ad56c6aa927d56562b32e44c2e5816c
antoniopagotto121@gmail.com <antoniopagotto121@gmail.com>
2021-01-02 15:31:10 -0300

    validando com propertyBinding (treinando...)

--- angular/projetos/app2/src/app/layout/pages/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/layout/pages/ordem-compra/ordem-compra.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  endereco: string = 'Rua XYZ';
-  numero: number = 10;
-  complemento: string = 'casa rosa';
-  formaPagamento: string = 'debito';
+  endereco: string = 'Endereço';
+  numero: number = 0;
+  complemento: string = 'Complemento';
+  formaPagamento: string;
+
+  //validando campos
+  enderecoValido: boolean;
+  numeroValido: boolean;
+  complementoValido: boolean;
+  formaPagamentoValido: boolean;
+
+  //estados primitivos dos campos
+  enderecoPrimitivo: boolean = true;
+  numeroPrimitivo: boolean = true;
+  complementoPrimitivo: boolean = true;
+  formaPagamentoPrimitivo: boolean = true;
 
   constructor() { }
 
@@ -18,15 +30,40 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   atualizaEndereco(endereco: string){
+    this.enderecoPrimitivo = false;
     this.endereco = endereco;
+    if(this.endereco.length > 5){
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
+
   atualizaNumero(numero: number){
+    this.numeroPrimitivo = false;
     this.numero = numero;
+    if(this.numero > 0){
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
+
   atualizaComplemento(complemento: string){
+    this.complementoPrimitivo = false;
     this.complemento = complemento;
+    if(this.complemento.length > 0){
+      this.complementoValido = true;
+    }
   }
+
   atualizaFormaPagamento(formaPagamento: string){
+    this.formaPagamentoPrimitivo = false;
     this.formaPagamento = formaPagamento;
+    if(this.formaPagamento.length > 0){
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 }
